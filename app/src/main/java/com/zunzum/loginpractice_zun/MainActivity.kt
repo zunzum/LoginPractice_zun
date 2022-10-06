fe04e3bd96b3e03bcd5cc0d0eedd9c2b8a7aeeb3
zunzum <zunzum@naver.com>
2022-10-06 20:26:33 +0900

    스스로 해냄 작고 소중한 내 첫 어플

---
+++ app/src/main/java/com/zunzum/loginpractice_zun/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.zunzum.loginpractice_zun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+            val inputId = idCheck.text.toString()
+            val inputPass = passCheck.text.toString()
+            if ( inputId=="admin" && inputPass=="qwer") {
+                Toast.makeText(this, "관리자입니다", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
